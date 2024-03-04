4fc555c79141e1e385d00e72c968d511d25b79e1
99narii <122865149+99narii@users.noreply.github.com>
2024-03-04 19:14:26 +0900

    반응형 오늘의 딜

--- main.js
+++ main.js
@@ -47,4 +47,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //글쓰기 버튼을 누르면 리스트들이 보이기
 
+document.querySelector('.header-upper-writeBtn').addEventListener('click', function() {
+  var lists = document.querySelector('.header-upper-lists');
+  if (lists.style.display === 'block') {
+    lists.style.display = 'none';
+  } else {
+    lists.style.display = 'block';
+  }
+});
+
+
 //하단 헤더에서 더보기를 누르면 리스트로 보이기
+
+//카테고리 캐러셀
+
+document.addEventListener("DOMContentLoaded", function() {
+  const categoryList = document.querySelector(".category-list ul");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  let scrollAmount = 0;
+
+  //초기 prev 버튼 숨김
+  prevBtn.style.display = 'none';
+
+  nextBtn.addEventListener("click", function() {
+    scrollAmount += 500; 
+    categoryList.style.transform = `translateX(-${scrollAmount}px)`;
+
+    // 스크롤이 시작되면 prev btn 보임
+    if (scrollAmount > 0) {
+      prevBtn.style.display = 'block';
+    }
+  });
+
+  prevBtn.addEventListener("click", function() {
+    scrollAmount -= 500; 
+    scrollAmount = Math.max(0, scrollAmount); 
+    categoryList.style.transform = `translateX(-${scrollAmount}px)`;
+
+    // 스크롤 위치가 0이면 prev btn 숨김
+    if (scrollAmount === 0) {
+      prevBtn.style.display = 'none';
+    }
+  });
+});
+
+
+
+
